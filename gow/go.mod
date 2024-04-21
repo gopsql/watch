bcb6ed3840a80df5c9e29e2463077fa3f62fc09c
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2024-04-21 22:16:04 +0800

    Add -prebuild

---
+++ gow/go.mod
@@ -0,0 +1,11 @@
+module gow
+
+go 1.20
+
+require (
+	github.com/gopsql/logger v1.0.0
+	github.com/gopsql/watch v1.2.1
+	github.com/mattn/go-shellwords v1.0.12
+)
+
+require github.com/radovskyb/watcher v1.0.7 // indirect
--- gow/main.go
+++ gow/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
 	"github.com/gopsql/logger"
 	"github.com/gopsql/watch"
+	"github.com/mattn/go-shellwords"
 )
 
 type list []string
@@ -26,6 +28,7 @@ func main() {
 	var noRun bool
 	var isTest bool
 	var goClean bool
+	var prebuildStr string
 	var changeDir string
 	var rebuildKeyStr string
 	ignore := list{"node_modules", ".git", "dist"}
@@ -35,6 +38,7 @@ func main() {
 	flag.BoolVar(&noRun, "no-run", false, "do not run the executable after go build")
 	flag.BoolVar(&isTest, "test", false, "run go test instead of go build")
 	flag.BoolVar(&goClean, "clean", false, "run go clean -cache or -testcache (if -test) first")
+	flag.StringVar(&prebuildStr, "prebuild", "", "run command before go build or go test")
 	flag.StringVar(&changeDir, "cd", "", "set working directory of commands")
 	flag.StringVar(&rebuildKeyStr, "rebuild-key", "r", "key to rebuild")
 	flag.Var(&ignore, "ignore", "add extra directory name to ignore")
@@ -71,6 +75,15 @@ func main() {
 		}
 	}
 
+	var prebuild []string
+	if prebuildStr != "" {
+		var err error
+		prebuild, err = shellwords.Parse(prebuildStr)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
 	var output string
 	for i := len(goBuildArgs) - 2; i > -1; i-- {
 		if goBuildArgs[i] == "-o" || goBuildArgs[i] == "--o" {
@@ -84,6 +97,7 @@ func main() {
 		SetNoRun(noRun).
 		SetTest(isTest).
 		SetClean(goClean).
+		SetPrebuild(prebuild...).
 		ChangeDirectory(changeDir).
 		WithOutput(output).
 		WithGoPath(goPath).
